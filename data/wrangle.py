68ad5bdd5a1a263504fe43612f35abed0221e57b
Matias Frank Jensen <matias@nuntio.io>
2024-01-16 22:07:54 +0100

    datawrangle

--- data/wrangle.py
+++ data/wrangle.py
@@ -58,6 +58,20 @@ def capital_fixup():
     capitals["Dominican Republic"] = "Santo Domingo"
     capitals["Guatemala"] = "Guatemala City"
     capitals["Cook Islands"] = "Avarua"
+    capitals["China"] = "Beijing"
+    capitals["Bahrain"] = "Manama"
+    capitals["Mongolia"] = "Ulaanbaatar"
+    capitals["Oman"] = "Muscat"
+    capitals["Uzbekistan"] = "Tashkent"
+    capitals["Austria"] = "Vienna"
+    capitals["Czech Republic"] = "Prague"
+    capitals["Faroe Islands"] = "Torshavn"
+    capitals["Greece"] = "Athens"
+    capitals["Monaco"] = "Monaco"
+    capitals["Italy"] = "Rome"
+    capitals["Romania"] = "Bucharest"
+    capitals["Portugal"] = "Lisbon"
+    capitals["Poland"] = "Warsaw"
 
     capitals = [{"country": k, "city": v} for k, v in capitals.items()]
     capitals.sort(key=lambda x: x["country"])
@@ -98,7 +112,7 @@ def continent_capitals_questions():
         ]
         items.sort(key=lambda x: x["question"])
         questions = {
-            "name": "capitals",
+            "name": continent + "_capitals",
             "type_": "default",
             "data": {
                 "question_prefix": "What is the capital of ",
@@ -109,11 +123,66 @@ def continent_capitals_questions():
             dump(questions, f, Dumper=Dumper)
 
 
+def continent_to_areas():
+    with open("continent_to_country.json") as f:
+        continents = json.load(f)
+    with open("country_area.json") as f:
+        areas = json.load(f)
+    min_area = 1000
+    areas = [item for item in areas if item["area"] > min_area]
+    # printable = set(string.printable)
+    # fprint = lambda s: "".join(ss for ss in s if ss in printable)
+    for continent, countries in continents.items():
+        continent_areas = [item for item in areas if item["country"] in countries]
+        if len(continent_areas) == 0:
+            continue
+        continent = continent.replace(" ", "_").lower()
+        with open(f"{continent}_areas.json", "w") as f:
+            json.dump(continent_areas, f, indent=2)
+
+
+def continent_area_questions():
+    for p in os.listdir("."):
+        if not p.endswith("_areas.json"):
+            continue
+        with open(p) as f:
+            data = json.load(f)
+        continent = p.removesuffix("_areas.json")
+        items = [
+            {
+                "question": d["country"],
+                "answer": int(d["area"]),
+            }
+            for d in data
+        ]
+        items.sort(key=lambda x: x["question"])
+        questions = {
+            "name": continent + "_areas",
+            "type_": "numeric_range",
+            "data": {
+                "question_prefix": "What is the area (km^2) of ",
+                "items": items,
+                "range": 0.025,
+            },
+        }
+        with open(f"questions/{continent}_areas.yaml", "w") as f:
+            dump(questions, f, Dumper=Dumper)
+
+
 def gen_capital_questions():
     continent_fixup()
     split_by_continent()
-    capital_fixup()    
+    capital_fixup()
     continent_to_capitals()
     continent_capitals_questions()
 
+
+def gen_area_questions():
+    continent_fixup()
+    split_by_continent()
+    continent_to_areas()
+    continent_area_questions()
+
+
 gen_capital_questions()
+gen_area_questions()
