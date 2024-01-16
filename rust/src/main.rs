0627f61b12edd1da8d83f5c481276ec0468fc65f
Matias Frank Jensen <matias@nuntio.io>
2024-01-16 22:07:32 +0100

    more rust stuff:

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,13 @@
-use anyhow::{Error, Result};
+use anyhow::{bail, Error, Result};
 use clap::Parser;
+use inquire::validator::{ErrorMessage, Validation};
 use inquire::Text;
+use num_format::{Locale, ToFormattedString};
 use rand::{seq::SliceRandom, thread_rng};
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::fmt::Debug;
+use std::io::{stdin, stdout, Read, Write};
+use std::{collections::HashMap, fs};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -11,15 +15,7 @@ use std::fs;
 struct Args {
     /// Path to the question set
     #[arg(short, long)]
-    paths: Vec<String>,
-
-    /// Question start index (inclusive, 1-indexed)
-    #[arg(short, long, default_value_t = 1)]
-    start: usize,
-
-    /// Question end index (inclusive, 1-indexed)
-    #[arg(short, long, default_value_t = 1_000_000)]
-    end: usize,
+    path: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -36,13 +32,107 @@ struct QuestionSet<T> {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+struct NumericRangeData {
+    items: Vec<NumericRangeQuestion>,
+    question_prefix: String,
+    range: f64,
+}
+
+impl QuestionFactory for NumericRangeData {
+    fn build(
+        &self,
+        _: &HashMap<String, Box<dyn QuestionFactory>>,
+    ) -> Result<Vec<Box<dyn Question>>> {
+        let mut result = Vec::new();
+        for item in &self.items {
+            let mut question = item.clone();
+            question.range = self.range;
+            question.question = format!("{}{}?", self.question_prefix, question.question);
+            result.push(Box::new(question) as Box<dyn Question>);
+        }
+        Ok(result)
+    }
+}
+
+fn default_range() -> f64 {
+    0.
+}
+
+fn si_parse(s: &str) -> Result<i64> {
+    let c = if let Some(c) = s.chars().last() {
+        c
+    } else {
+        bail!("empty string");
+    };
+    if c.is_digit(10) {
+        return s.parse::<i64>().map_err(|err| anyhow::Error::from(err));
+    }
+
+    let factor: i64 = match c {
+        'k' | 'K' => 1_000,
+        'm' | 'M' => 1_000_000,
+        'g' | 'G' | 'b' | 'B' => 1_000_000_000,
+        'T' => 1_000_000_000_000,
+        _ => bail!("unexpected last char {}", c),
+    };
+    let ss = s.get(..s.len() - 1).unwrap();
+    let n = (ss.parse::<f64>()? * (factor as f64)) as i64;
+    Ok(n)
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct NumericRangeQuestion {
+    question: String,
+    answer: i64,
+    #[serde(default = "default_range")]
+    range: f64,
+}
+
+impl Question for NumericRangeQuestion {
+    fn run(&self) -> Result<bool> {
+        let validator = |input: &str| match si_parse(input) {
+            Ok(_) => Ok(Validation::Valid),
+            Err(err) => Ok(Validation::Invalid(ErrorMessage::Custom(format!(
+                "{:?}",
+                err
+            )))),
+        };
+
+        let answer = Text::new(&self.question)
+            .with_validator(validator)
+            .prompt()?;
+
+        let min = ((self.answer as f64) * (1. - self.range)) as i64;
+        let max = ((self.answer as f64) * (1. + self.range)) as i64;
+        let a = si_parse(&answer)?;
+        let correct = min <= a && a <= max;
+        let (min_s, area_s, max_s) = (
+            min.to_formatted_string(&Locale::en),
+            self.answer.to_formatted_string(&Locale::en),
+            max.to_formatted_string(&Locale::en),
+        );
+        let bound = format!("[{} <= {} <= {}]", min_s, area_s, max_s);
+        if correct {
+            println!("Within accepted bounds! {}", bound);
+        } else {
+            println!("Wrong. Accepted bounds: {}", bound);
+        }
+        println!("");
+        Ok(correct)
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 struct DefaultData {
     items: Vec<DefaultQuestion>,
     question_prefix: String,
 }
 
 impl QuestionFactory for DefaultData {
-    fn build(&self) -> Result<Vec<Box<dyn Question>>> {
+    fn build(
+        &self,
+        _: &HashMap<String, Box<dyn QuestionFactory>>,
+    ) -> Result<Vec<Box<dyn Question>>> {
         let mut result = Vec::new();
         for item in &self.items {
             let mut question = item.clone();
@@ -71,7 +161,37 @@ impl Question for DefaultQuestion {
         } else {
             println!("Wrong. The answer is {:?}", self.answers[0]);
         }
+        println!("");
         Ok(correct)
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct UnionData {
+    sets: Vec<String>,
+}
+
+struct UnionDataFactory {
+    data: UnionData,
+}
+
+impl QuestionFactory for UnionDataFactory {
+    fn build(
+        &self,
+        all_factories: &HashMap<String, Box<dyn QuestionFactory>>,
+    ) -> Result<Vec<Box<dyn Question>>> {
+        let mut res = Vec::new();
+
+        for name in &self.data.sets {
+            if let Some(factory) = all_factories.get(name) {
+                let questions = factory.build(all_factories)?;
+                res.extend(questions);
+            } else {
+                bail!("unknown question set {}", name);
+            }
+        }
+
+        Ok(res)
     }
 }
 
@@ -80,59 +200,109 @@ trait Question {
 }
 
 trait QuestionFactory {
-    fn build(&self) -> Result<Vec<Box<dyn Question>>>;
+    fn build(
+        &self,
+        all_factories: &HashMap<String, Box<dyn QuestionFactory>>,
+    ) -> Result<Vec<Box<dyn Question>>>;
+}
+
+fn pause() -> Result<()> {
+    let mut stdout = stdout();
+    stdout.write(b"Press any key to continue...")?;
+    stdout.flush().unwrap();
+    stdin().read(&mut [0])?;
+    Ok(())
 }
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let mut questions = Vec::new();
-    for path in args.paths {
-        let contents = fs::read_to_string(path)?;
-
+    let mut factories = HashMap::new();
+    let paths = fs::read_dir(args.path)?;
+    for path in paths {
+        let p = path?.path();
+        let contents = fs::read_to_string(p)?;
         let base: BaseQuestionSet = serde_yaml::from_str(&contents)?;
-
-        let factory: Box<dyn QuestionFactory> = match base.type_.as_str() {
+        let (factory, name) = match base.type_.as_str() {
             "default" => {
                 let set = serde_yaml::from_str::<QuestionSet<DefaultData>>(&contents)?;
-                Box::new(set.data) as Box<dyn QuestionFactory>
+                (Box::new(set.data) as Box<dyn QuestionFactory>, set.name)
+            }
+            "numeric_range" => {
+                let set = serde_yaml::from_str::<QuestionSet<NumericRangeData>>(&contents)?;
+                (Box::new(set.data) as Box<dyn QuestionFactory>, set.name)
+            }
+            "union" => {
+                let set = serde_yaml::from_str::<QuestionSet<UnionData>>(&contents)?;
+                (
+                    Box::new(UnionDataFactory { data: set.data }) as Box<dyn QuestionFactory>,
+                    set.name,
+                )
             }
             _ => {
                 panic!("unexpected question type {:?}", base.type_)
             }
         };
-
-        questions.extend(factory.build()?);
+        factories.insert(name, factory);
     }
-    let end = std::cmp::min(args.end, questions.len());
-    let mut question_slice: Vec<&Box<dyn Question>> =
-        questions[args.start - 1..end].iter().map(|q| q).collect();
-    question_slice.shuffle(&mut thread_rng());
 
-    let mut wrong = Vec::new();
     loop {
-        for (i, &q) in question_slice.iter().enumerate() {
-            print!("{}/{}: ", i + 1, question_slice.len());
-            let correct = q.run()?;
-            if !correct {
-                wrong.push(q);
-            }
-        }
-
-        if wrong.is_empty() {
+        let mut options: Vec<String> = factories.keys().map(|s| s.clone()).collect();
+        options.sort();
+        options.insert(0, String::from("Exit"));
+        let select = inquire::Select::new("Pick a question set", options);
+        let choice = select.prompt()?;
+        if choice == "Exit" {
             break;
         }
+        let factory = factories.get(&choice).unwrap();
+        let questions = factory.build(&factories)?;
 
-        let num_correct = question_slice.len() - wrong.len();
-        println!(
-            "\n{}/{} correct. Continuing with the remaining {} wrong answers.",
-            num_correct,
-            question_slice.len(),
-            wrong.len()
-        );
-        std::mem::swap(&mut wrong, &mut question_slice);
-        wrong.clear();
+        let start = inquire::Text::new("Start index:")
+            .with_initial_value("1")
+            .prompt()?
+            .parse::<usize>()?;
+        let end = inquire::Text::new("End index:")
+            .with_initial_value(&format!("{}", questions.len()))
+            .prompt()?
+            .parse::<usize>()?;
+        let mut question_slice: Vec<&Box<dyn Question>> =
+            questions[start - 1..end].iter().map(|q| q).collect();
         question_slice.shuffle(&mut thread_rng());
+
+        clearscreen::clear()?;
+        let mut wrong = Vec::new();
+        loop {
+            for (i, &q) in question_slice.iter().enumerate() {
+                print!("{}/{}: ", i + 1, question_slice.len());
+                let correct = q.run()?;
+                if !correct {
+                    wrong.push(q);
+                }
+            }
+
+            if wrong.is_empty() {
+                break;
+            }
+
+            let num_correct = question_slice.len() - wrong.len();
+            println!(
+                "\n{}/{} correct. Continuing with the remaining {} wrong answers.",
+                num_correct,
+                question_slice.len(),
+                wrong.len()
+            );
+
+            std::mem::swap(&mut wrong, &mut question_slice);
+            wrong.clear();
+            question_slice.shuffle(&mut thread_rng());
+
+            pause()?;
+            clearscreen::clear()?;
+        }
+
+        pause()?;
+        clearscreen::clear()?;
     }
 
     Ok(())
