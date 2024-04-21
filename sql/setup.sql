a84bb8674e1994e67b539f3090132a15105175ef
Matias Frank Jensen <matias@nuntio.io>
2024-04-21 21:42:32 +0200

    SQL setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS questions (
     id INTEGER PRIMARY KEY,
-    question_set TEXT NOT NULL,
+    factory TEXT NOT NULL,
     name TEXT NOT NULL,
     created_at INTEGER NOT NULL,
     last_answered_at INTEGER,
@@ -8,8 +8,9 @@ CREATE TABLE IF NOT EXISTS questions (
     num_correct INTEGER NOT NULL,
     num_incorrect INTEGER NOT NULL,
     data BLOB NOT NULL,
-    UNIQUE(question_set, name)
+    UNIQUE(factory, name)
 );
+CREATE INDEX IF NOT EXISTS index_questions ON questions(factory, name);
 
 CREATE TABLE IF NOT EXISTS answers (
     id INTEGER PRIMARY KEY,
@@ -17,11 +18,19 @@ CREATE TABLE IF NOT EXISTS answers (
     time INTEGER,
     correct INTEGER
 );
+CREATE INDEX IF NOT EXISTS index_answers ON answers(question_id, time);
 
 CREATE TABLE IF NOT EXISTS question_sets (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    set_type TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    UNIQUE(name, question_id)
+);
+
+CREATE TABLE IF NOT EXISTS question_factories (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    factory_type TEXT NOT NULL,
     data BLOB NOT NULL,
     UNIQUE(name)
 );
